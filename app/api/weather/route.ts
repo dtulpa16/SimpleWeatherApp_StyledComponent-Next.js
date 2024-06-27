2e3196961fe2862d8e332d4d5e85d27779effca9
Daniel Tulpa <dtulpa16@yahoo.com>
2024-06-27 09:00:45 -0600

    Added API Endpoint For Weather Data

---
+++ app/api/weather/route.ts
@@ -0,0 +1,22 @@
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const location = searchParams.get("location");
+
+  const url =`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${location}`;
+
+  const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': process.env.WEATHER_API_KEY!,
+            'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        return result
+    } catch (error) {
+        console.error(error);
+    }
+}
--- app/weather/[slug]/page.tsx
+++ app/weather/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-export default function WeatherDetailsPage() {
+export default async function WeatherDetailsPage({params}:{params:{slug:string}}) {
+  const location = params.slug || 'Chicago'
+  const data = await fetch(`/api/weather?loation=${location}`)
   return (
     <div>page</div>
   )
